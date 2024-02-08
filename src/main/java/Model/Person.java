e8bf083174f8e6bc9d0019b91fbcd6ff2640412a
VoldMarius <ukusil@mail.ru>
2024-02-08 15:39:33 +0300

    ADD dependency and two methods

--- src/main/java/Model/Person.java
+++ src/main/java/Model/Person.java
@@ -3,6 +3,9 @@ package Model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import java.io.*;
 @AllArgsConstructor
 @NoArgsConstructor
@@ -24,5 +27,28 @@ public class Person implements Externalizable {
         firstName = (String) in.readObject();
         lastName = (String) in.readObject();
         age = in.readInt();
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return new EqualsBuilder()
+                .append(age, person.age)
+                .append(firstName, person.firstName)
+                .append(lastName, person.lastName)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(firstName)
+                .append(lastName)
+                .append(age)
+                .toHashCode();
     }
 }
